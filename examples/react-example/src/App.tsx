3309e84f5f5cbdfc736beaa2bce5a3d671413812
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-02-01 01:44:32 +0530

    basic react icons setup is done

---
+++ examples/react-example/src/App.tsx
@@ -0,0 +1,14 @@
+import { House, CloudSun, CloudSunFill, HouseFill } from "react-icons";
+
+function App() {
+  return (
+    <div>
+      <House />
+      <CloudSun />
+      <CloudSunFill />
+      <HouseFill />
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/react-icons/index.tsx
@@ -0,0 +1,4 @@
+export * from "./fill/CloudSunFill";
+export * from "./fill/HouseFill";
+export * from "./outline/CloudSun";
+export * from "./outline/House";
