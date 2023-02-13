30f3b3c4125c8d24c1edda7a61a894ba9e3c1d6c
sahilshahane <sahilpshahane123@gmail.com>
2023-02-14 00:45:56 +0530

    example update

--- examples/next-ts-example/pages/index.tsx
+++ examples/next-ts-example/pages/index.tsx
@@ -1,21 +1,43 @@
-import { ActivityFill, JengaIconContext } from '@jengaicons/react'
+import {
+  JengaIconContext,
+  ActivityFill,
+  Activity,
+  Cube,
+} from '@jengaicons/react'
 
 export default function Home() {
   return (
     <>
-      <main>
-        <JengaIconContext.Provider
-          value={{
-            alt: 'yoyo',
-            color: 'red',
-            mirrored: false,
-          }}
-        >
+      <JengaIconContext.Provider
+        value={{
+          color: 'red',
+        }}
+      >
+        <main>
+          <ActivityFill size={300} />
           <div style={{ color: 'green' }}>
-            <ActivityFill />
+            <Activity weight={0.5} size='300px' color='currentColor' />
           </div>
-        </JengaIconContext.Provider>
-      </main>
+
+          <Cube color='blue' size={300} weight='1'>
+            <animate
+              attributeName='opacity'
+              values='0;1;0'
+              dur='4s'
+              repeatCount='indefinite'
+            ></animate>
+            <animateTransform
+              attributeName='transform'
+              attributeType='XML'
+              type='rotate'
+              dur='5s'
+              from='0 0 0'
+              to='360 0 0'
+              repeatCount='indefinite'
+            ></animateTransform>
+          </Cube>
+        </main>
+      </JengaIconContext.Provider>
     </>
   )
 }
