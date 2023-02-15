913fbbf9e3c4da310ab6a182973dce5e7f59308b
sahilbest999 <sahilpshahane123@gmail.com>
2023-02-15 18:18:50 +0530

    fix formatting

--- packages/jengaicons-react/icons/fill/ActivityFill.tsx
+++ packages/jengaicons-react/icons/fill/ActivityFill.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react'
-import { forwardRef, useContext } from 'react'
-import { JengaIconContext } from '../../src/base'
-import type { JengaIconProps } from '../../src/base'
+import * as React from "react";
+import { forwardRef, useContext } from "react";
+import { JengaIconContext } from "../../src/base";
+import type { JengaIconProps } from "../../src/base";
 
 const ActivityFill = forwardRef<SVGSVGElement, JengaIconProps>((props, ref) => {
-  const { size, color, alt, children, mirrored } = props
+  const { size, color, alt, children, mirrored } = props;
 
   const {
     alt: altCtx,
@@ -12,33 +12,33 @@ const ActivityFill = forwardRef<SVGSVGElement, JengaIconProps>((props, ref) => {
     color: colorCtx,
     mirrored: mirroredCtx,
     size: sizeCtx,
-  } = useContext(JengaIconContext)
+  } = useContext(JengaIconContext);
 
   return (
     <svg
       width={size || sizeCtx || 32}
       height={size || sizeCtx || 32}
-      transform={mirrored || mirroredCtx ? 'scale(-1, 1)' : undefined}
+      transform={mirrored || mirroredCtx ? "scale(-1, 1)" : undefined}
       ref={ref}
-      xmlns='http://www.w3.org/2000/svg'
-      fill='none'
-      viewBox='0 0 32 32'
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 32 32"
     >
       {(!!altCtx || !!alt) && <title>{alt || altCtx}</title>}
 
       <path
-        stroke={color || colorCtx || '#000000'}
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        strokeWidth='2'
-        d='M4.96 16.744h3.692l4.616-10.153 7.384 19.384 4.616-9.23h3.692'
+        stroke={color || colorCtx || "#000000"}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M4.96 16.744h3.692l4.616-10.153 7.384 19.384 4.616-9.23h3.692"
       />
 
       {children || childrenCtx}
     </svg>
-  )
-})
+  );
+});
 
-ActivityFill.displayName = 'ActivityFill'
+ActivityFill.displayName = "ActivityFill";
 
-export default ActivityFill
+export default ActivityFill;
