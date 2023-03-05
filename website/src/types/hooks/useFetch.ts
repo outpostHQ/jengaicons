cbe7cd01551d3681d65d1ff4d7dcccad2867ec91
sahilbest999 <sahilpshahane123@gmail.com>
2023-03-06 02:00:51 +0530

    feat(website): create `useFetch` hook

---
+++ website/src/types/hooks/useFetch.ts
@@ -0,0 +1,6 @@
+export type Loading = boolean
+
+export type IuseFetch = <Response = undefined, Error = undefined>(obj: {
+  url: string
+  type: 'GET' | 'POST'
+}) => Readonly<[Loading, Response, Error]>
