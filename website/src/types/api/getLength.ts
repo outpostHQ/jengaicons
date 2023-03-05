9ba916af16dad6d271694007a0cf1d4823ddcef9
sahilbest999 <sahilpshahane123@gmail.com>
2023-03-06 02:13:33 +0530

    feat(website): added `getIconLength` API

---
+++ website/src/types/api/getLength.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { TVariants } from '@/types/index'
+
+export type NextHandler<ReqBody = {}, ResBody = {}> = (
+  req: NextApiRequest & { body?: ReqBody },
+  res: NextApiResponse<ResBody>
+) => void
+
+export type GetLengthResponse = {
+  [variant in TVariants]: number
+}
+export type GetLengthHandler = NextHandler<undefined, GetLengthResponse>
