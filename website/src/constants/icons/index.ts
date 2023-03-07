e8a0ae174225f4cbfa5eb051b52b5e63b749b5ca
sahilbest999 <sahilpshahane123@gmail.com>
2023-03-07 17:20:56 +0530

    chore: unified ouside imports

---
+++ website/src/constants/icons/index.ts
@@ -0,0 +1,7 @@
+import RegularIconMetaData from '../../../../optimized/regular.json'
+import FillIconMetaData from '../../../../optimized/fill.json'
+import AllIconMetaData from '../../../../optimized/combined-icons.json'
+
+
+
+export {AllIconMetaData, FillIconMetaData, RegularIconMetaData}
