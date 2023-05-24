f039735c5df531180290c4ccb73f38c356ee6648
sahilbest999 <sahilpshahane123@gmail.com>
2023-05-24 09:33:17 +0530

    feat: example update

--- examples/next-ts-example/pages/index.tsx
+++ examples/next-ts-example/pages/index.tsx
@@ -7,35 +7,37 @@ import {
 
 export default function Home() {
   return (
-    <JengaIconContext.Provider
-      value={{
-        color: "var(--icn-color)", // or color: 'red',
-      }}
-    >
-      <main>
-        <ActivityFill size={300} />
-        <div style={{ color: "green" }}>
-          <Activity weight={0.5} size='300px' color='currentColor' />
-        </div>
+    <main>
+      <div style={{ color: "green" }}>
+        <Activity weight={"0.5px"} size='300px' />
+      </div>
+      <JengaIconContext.Provider
+        value={{
+          color: "red",
+        }}
+      >
+        <div>
+          <ActivityFill size={300} />
 
-        <Cube size={300} weight='1'>
-          <animate
-            attributeName='opacity'
-            values='0;1;0'
-            dur='4s'
-            repeatCount='indefinite'
-          />
-          <animateTransform
-            attributeName='transform'
-            attributeType='XML'
-            type='rotate'
-            dur='5s'
-            from='0 0 0'
-            to='360 0 0'
-            repeatCount='indefinite'
-          />
-        </Cube>
-      </main>
-    </JengaIconContext.Provider>
+          <Cube size={300} alt='Just a cube spinning :)'>
+            <animate
+              attributeName='opacity'
+              values='0;1;0'
+              dur='4s'
+              repeatCount='indefinite'
+            />
+            <animateTransform
+              attributeName='transform'
+              attributeType='XML'
+              type='rotate'
+              dur='5s'
+              from='0 0 0'
+              to='360 0 0'
+              repeatCount='indefinite'
+            />
+          </Cube>
+        </div>
+      </JengaIconContext.Provider>
+    </main>
   )
 }
