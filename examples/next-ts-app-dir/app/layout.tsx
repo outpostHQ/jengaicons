4614ee909c39a180601a3a6b4440c760c4b1121e
sahilbest999 <sahilpshahane123@gmail.com>
2023-08-30 12:25:15 +0530

    fix: nextjs app dir build errors

--- examples/next-ts-app-dir/app/layout.tsx
+++ examples/next-ts-app-dir/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { JengaIconsContextProvider } from "./_components/JengaContextProvider"
-import "@/styles/global.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
