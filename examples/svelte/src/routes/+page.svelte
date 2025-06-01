9c24b8f7e09e319be064c469e0ff1b9b56dc2c9c
sahilshahane <sahilpshahane123@gmail.com>
2025-06-01 18:30:16 +0530

    feat: svelte example

---
+++ examples/svelte/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Icon from '../icons/icons/Activity.svelte'
+	// import {AArrowDown} from 'lucide-svelte'
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Icon color="blue" size={70}/>
+
+<!-- <AArrowDown color="red" /> -->
