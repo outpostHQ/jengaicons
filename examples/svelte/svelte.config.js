0267a97feccdac2c3a6de60a4a51510d98fd9e98
shubham-kaushal <nsk9195@gmail.com>
2026-02-03 16:23:45 +0530

    fix: add runes support

--- examples/svelte/svelte.config.js
+++ examples/svelte/svelte.config.js
@@ -13,6 +13,9 @@ const config = {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
   },
+  compilerOptions: {
+    runes: true,
+  },
 }
 
 export default config
