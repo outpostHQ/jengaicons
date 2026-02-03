3ceb5343647db19a11c52cd4dbf7523494654ff3
Kamal Singh <itskamal.dev@gmail.com>
2026-02-03 14:33:27 +0530

    fix: update export statements

--- scripts/generate-jengaicons-react/index.ts
+++ scripts/generate-jengaicons-react/index.ts
@@ -127,7 +127,7 @@ async function main() {
   fs.appendFileSync(
     PATH_TO_SRC_INDEX_FILE,
     [
-      `export { JengaIconContext } from "./context";`,
+      `export { JengaIconContext, default as JengaIconContextDefault } from "./context";`,
       `export type *  from "./types"`,
       `export *  from "./nextjs"`,
     ].join("\n"),
